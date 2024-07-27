c40dc4e737aadc2089245e330134c8504e51e044
Zane Rockenbaugh <zane@liquid-labs.com>
2024-07-27 16:56:48 -0500

    remove unused library file

--- src/lib/normalize-email.js
+++
@@ -1,8 +0,0 @@
-const normalizeEmail = function (emailsOrText, {
-  leaveComment = this?.leaveComment || false,
-  leaveDisplayName = this?.leaveDisplayName || false
-} = {}) {
-
-}
-
-export { normalizeEmail }
