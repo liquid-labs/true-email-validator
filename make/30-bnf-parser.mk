ee9d37ef19be1c6ab89b3bc6c00b6e00e0feb512
Zane Rockenbaugh <zane@liquid-labs.com>
2024-07-30 18:05:22 -0500

    install the necessary packages to build BNF parser

--- make/30-bnf-parser.mk
+++ make/30-bnf-parser.mk
@@ -4,6 +4,7 @@ BNF_PARSER_SRC:=$(shell find $(BNF_PARSER_PATH) -name "*.js" -name "*.ts" -name 
 BUILD_TARGETS+=$(BNF_PARSER)
 
 $(BNF_PARSER): $(BNF_PARSER_SRC)
+	cd $(BNF_PARSER_PATH) && npm i
 	cd $(BNF_PARSER_PATH) && npm run build
 	{ ! [ -L tools/bnf-parser ] && cd tools && ln -s ../$(BNF_PARSER_PATH) bnf-parser; } || true
 	mv tools/bnf-parser/bin/cli.js tools/bnf-parser/bin/cli.mjs
